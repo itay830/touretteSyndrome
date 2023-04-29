1ffbe4bdb4d94a1e23f52cf9c7042b54f37faa81
Itay <itay.shinderman@gmail.com>
2023-04-29 11:48:55 +0300

    new things

--- main.py
+++ main.py
@@ -4,29 +4,55 @@ import time
 
 pygame.init()
 
-WIDTH, HEIGHT = 1200, 800
+WIDTH, HEIGHT = 1400, 900
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("My-Special-Classroom")
+pygame.mouse.set_visible(False)
 clock = pygame.time.Clock()
 FPS = 60
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+CENTER = (WIDTH/2, HEIGHT/2)
+classroomBg = pygame.image.load('resources/bgs/classroom.png').convert_alpha()
 
+mouse_current_img = mouseArrow = pygame.image.load('resources/mouse/arrow.png').convert_alpha()
+mouseHand = pygame.image.load('resources/mouse/hand.png').convert_alpha()
+
+class Mouse(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.images = [pygame.image.load('resources/mouse/arrow.png').convert_alpha(), pygame.image.load('resources/mouse/hand.png').convert_alpha()]
+        self.rect = self.images[0].get_rect()
+
+    def update(self):
+        self.rect.topleft = pygame.mouse.get_pos()
+        screen.blit(self.image, self.rect)
+
+mouse = Mouse()
 
 class App:
     def __init__(self):
         self.gameState = 'main menu'
 
+    @staticmethod
+    def regular_events():
+        for e in pygame.event.get():
+            if e.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+
+
+
+
+
 
 class Button:
 
-    def __init__(self, pos, width, height, color):
+    def __init__(self, pos, width, height):
         self.surf = pygame.Surface((width, height))
-        self.color = color
         self.surf.set_colorkey(BLACK)
         self.rect = self.surf.get_rect(center=pos)
-        pygame.draw.ellipse(self.surf, self.color, (0, self.surf.get_height()/2, self.surf.get_width(), self.surf.get_height()/100 * 1))
 
         self.clickStage = None
         self.held = False
@@ -35,40 +61,53 @@ class Button:
         screen.blit(self.surf, self.rect)
 
     def logic(self):
-        if self.rect.collidepoint(pygame.mouse.get_pos()):
-            self.clickStage = 0
+        if self.rect.colliderect(mouse.rect):
+            self.clickStage = False
             self.surf.fill((255, 0, 0))
             if pygame.mouse.get_pressed()[0]:
                 self.held = True
                 self.surf.fill((0, 255, 0))
             if self.held and not pygame.mouse.get_pressed()[0]:
-                self.clickStage = 1
+                self.clickStage = True
 
         else:
             self.clickStage = False
             self.held = False
-            #pygame.draw.ellipse(self.surf, self.color, (0, self.surf.get_height()/2, self.surf.get_width(), self.surf.get_height()/100 * 50))
+            self.surf.fill((255, 0, 255))
 
     def update(self):
         self.logic()
         self.draw()
 
 
-b = Button((200, 500), 200, 100, WHITE)
+
+
+b = Button(CENTER, 200, 100)
+lv1_b = Button((150, 100), 200, 100)
+
 app = App()
 while 1:
     clock.tick(FPS)
-
-    for e in pygame.event.get():
-        if e.type == pygame.QUIT:
-            pygame.quit()
-            exit()
+    app.regular_events()
 
 
     if app.gameState == 'main menu':
         screen.fill(BLACK)
-
         b.update()
+        if b.clickStage:
+            app.gameState = 'level menu'
 
+    if app.gameState == 'level menu':
+        screen.fill(BLACK)
+        lv1_b.update()
+        if lv1_b.clickStage:
+            app.gameState = 'lv1'
+
+    if app.gameState.startswith('lv'):
+        screen.blit(classroomBg, (0, 0))
+        if app.gameState == 'lv1':
+            ...
+
+    screen.blit(mouse_current_img, pygame.mouse.get_pos())
     pygame.display.update()
 
